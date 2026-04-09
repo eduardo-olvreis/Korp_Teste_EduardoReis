c489c20e6ed6e393e4c720ca02860afe68955745
Eduardo <eduardo.olvreis@gmail.com>
2026-04-09 20:45:00 -0300

    feat: adição da interface de repoistório da NotaFiscal

---
+++ backend/Korp_Teste_EduardoReis/Repositories/INotaFiscalRepository.cs
@@ -0,0 +1,12 @@
+﻿using Korp.Faturamento.API.Entities;
+
+namespace Korp.Faturamento.API.Repositories
+{
+    public interface INotaFiscalRepository
+    {
+        public Task<NotaFiscal> ObterNotaPorIdAsync(int id);
+        public Task<IEnumerable<NotaFiscal>> ObterTodasNotasAsync();
+        public Task<NotaFiscal> CriarNotaFiscal(NotaFiscal nota);
+        public Task<bool> AtualizarStatusParaFechadaAsync(int id);
+    }
+}
