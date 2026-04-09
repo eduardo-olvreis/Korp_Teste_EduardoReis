1f06b563941e50b0ccb679615534b06df2a4ef9c
Eduardo <eduardo.olvreis@gmail.com>
2026-04-09 13:58:49 -0300

    feat: configuraçao inicial do banco de dados e entidade de produto no estoque

---
+++ backend/Korp_Faturamento_API/Entities/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Korp.Estoque.API.Entities
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo 'Codigo' não pode ser vazio.")]
+        [MaxLength(30)]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "Campo 'Descricao' não pode ser vazio.")]
+        [MaxLength(100)]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Campo 'Saldo' não pode ser vazio")]
+        public int Saldo { get; set; }
+    }
+}
