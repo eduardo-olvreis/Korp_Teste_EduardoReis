76e1e58d5da2dd5707d36b4f029cd9dbba77c6d2
Eduardo <eduardo.olvreis@gmail.com>
2026-04-09 16:53:50 -0300

    feat: configuração do banco de dados e entidades de notas fiscais no faturamento

---
+++ backend/Korp_Teste_EduardoReis/Entities/NotaFiscal.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Korp.Faturamento.API.Entities
+{
+    public class NotaFiscal
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo 'NumeroSequencial' não pode ser vazio.")]
+        public int NumeroSequencial { get; set; }
+
+        [Required(ErrorMessage = "Campo 'Status' não pode ser vazio.")]
+        [MaxLength(10)]
+        public string Status { get; set; } = "Aberta";
+
+        public ICollection<ItemNotaFiscal> Itens { get; set; } = new List<ItemNotaFiscal>();
+    }
+}
