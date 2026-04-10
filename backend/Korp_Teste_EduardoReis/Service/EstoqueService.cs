1c57d583be13468083dab1c25d98eb36e06bb5b5
Eduardo <eduardo.olvreis@gmail.com>
2026-04-10 13:43:27 -0300

    feat: implementação da comunicação com o microserviço de estoque via HttpClient

---
+++ backend/Korp_Teste_EduardoReis/Service/EstoqueService.cs
@@ -0,0 +1,35 @@
+﻿using Korp.Faturamento.API.Entities;
+using System.Text;
+using System.Text.Json;
+
+namespace Korp.Faturamento.API.Service
+{
+    public class EstoqueService : IEstoqueService
+    {
+        private readonly HttpClient _httpClient;
+        public EstoqueService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<bool> BaixarEstoqueAsync(IEnumerable<ItemNotaFiscal> itens)
+        {
+            var payload = itens.Select(i => new
+            {
+                produtoId = i.ProdutoId,
+                quantidade = i.Quantidade
+            });
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var json = JsonSerializer.Serialize(payload, options);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            try
+            {
+                var response = await _httpClient.PostAsync("api/produtos/baixar-estoque", content);
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ backend/Korp_Teste_EduardoReis/Service/IEstoqueService.cs
@@ -0,0 +1,9 @@
+﻿using Korp.Faturamento.API.Entities;
+
+namespace Korp.Faturamento.API.Service
+{
+    public interface IEstoqueService
+    {
+        Task<bool> BaixarEstoqueAsync(IEnumerable<ItemNotaFiscal> itens);
+    }
+}
