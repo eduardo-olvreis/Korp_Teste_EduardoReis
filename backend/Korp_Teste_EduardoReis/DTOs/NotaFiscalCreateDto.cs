ad13b48e535d0d1164c941192b86157bef836548
Eduardo <eduardo.olvreis@gmail.com>
2026-04-10 10:12:12 -0300

    feat: adição das DTOs da NotaFiscal

---
+++ backend/Korp_Teste_EduardoReis/DTOs/NotaFiscalCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Korp.Faturamento.API.DTOs
+{
+    public class NotaFiscalCreateDto
+    {
+        [MinLength(1)]
+        public List<ItemNotaFiscalCreateDto> Itens { get; set; } = new();
+    }
+
+    public class ItemNotaFiscalCreateDto
+    {
+        [Range(1, int.MaxValue)]
+        public int ProdutoId { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Quantidade { get; set; }
+    }
+}
---
+++ backend/Korp_Teste_EduardoReis/DTOs/NotaFiscalResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using Korp.Faturamento.API.Entities;
+
+namespace Korp.Faturamento.API.DTOs
+{
+    public class NotaFiscalResponseDto
+    {
+        public int Id { get; set; }
+        public int NumeroSequencial { get; set; }
+        public string Status { get; set; }
+        public List<ItemNotaFiscalResponseDto> Itens { get; set; } = new();
+    }
+
+    public class ItemNotaFiscalResponseDto
+    {
+        public int Id { get; set; }
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
