5d00969b34dbe49ad6cfec62cc8d2ae005463a94
Eduardo <eduardo.olvreis@gmail.com>
2026-04-15 15:26:54 -0300

    feat: implementação de poder adicionar mais de um produto em uma nota fiscal no front

--- frontend/korp-web/src/app/components/nota-fiscal-cadastro/nota-fiscal-cadastro.ts
+++ frontend/korp-web/src/app/components/nota-fiscal-cadastro/nota-fiscal-cadastro.ts
@@ -35,21 +35,31 @@ export class NotaFiscalCadastro implements OnInit {
     });
   }
 
+  adicionarItem(): void {
+    this.nota.itens.push({ produtoId: null, quantidade: 1 });
+  }
+
+  removerItem(index: number): void {
+    if (this.nota.itens.length > 1) {
+      this.nota.itens.splice(index, 1);
+    }
+  }
+
   salvar(): void {
-    if (!this.nota.itens[0].produtoId) {
-      alert('Selecione um produto!');
+    const itensInvalidos = this.nota.itens.some(i => !i.produtoId);
+    if (itensInvalidos) {
+      alert('Por favor selecione o produto em todos os itens da nota');
       return;
     }
-
-  this.notaService.criarNota(this.nota as any).subscribe({
-    next: (notaCriada) => {
-        const num = notaCriada.numeroSequencial || notaCriada.numeroSequencial || 'nova';
-        alert(`Nota Fiscal #${num} emitida com sucesso!`);
+    this.notaService.criarNota(this.nota as any).subscribe({
+      next: (notaCriada) => {
+        const num = notaCriada.numeroSequencial || 'nova';
+        alert(`Nota Fiscal #${num} emitida com sucesso contendo ${this.nota.itens.length} itens!`);
         this.router.navigate(['/notas']);
       },
       error: (err) => {
         console.error(err);
-        alert('Erro ao emitir nota.');
+        alert('Erro ao emitir nota');
       }
     });
   }
